056078af992faa061ebb1270b25ae45e811c3dca
Chris Meller <chris@doesnthaveone.com>
2010-12-06 21:58:11 -0500

    Make sure jQuery is loaded on every theme page.
    Flesh out the page title function, which now handles individual posts and individual pages. More will probably be needed soon...
    
    Add the 'controller' functions for both my custom blocks - archives and fetching Flickr items.

--- theme.php
+++ theme.php
@@ -18,13 +18,109 @@
 			
 			include_once('HTML.php');
 			
+			Stack::add(' template_header_javascript', Site::get_url( 'scripts' ) . '/jquery.js', 'jquery' );
 			Stack::add( 'template_header_javascript', Site::get_url( 'theme' ) . '/js/main.js', 'cwm_main_js', 'jquery' );
 			
 		}
 		
 		public function theme_page_title ( ) {
 			
-			return 'Foo';
+			$title = Options::get( 'title' );
+			
+			if ( $this->request->display_entry && isset( $this->post ) ) {
+				$title = $this->post->title . ' | ' . $title;
+			}
+			else if ( $this->request->display_page && isset( $this->post ) ) {
+				$title = $this->post->title . ' | ' . $title;
+			}
+			
+			return $title;
+			
+		}
+		
+		public function filter_block_list ( $block_list ) {
+			
+			$block_list[ 'cwm_archives' ] = _t( 'CWM Archives' );
+			$block_list[ 'cwm_flickr' ] = _t( 'CWM Flickr' );
+			
+			return $block_list;
+			
+		}
+		
+		public function action_block_content_cwm_archives ( $block, $theme ) {
+			
+			if ( Cache::has( 'cwm:archives' ) ) {
+				$archives = Cache::get( 'cwm:archives' );
+			}
+			else {
+				$archives = Posts::get( array( 'month_cts' => true, 'limit' => 8 ) );
+				
+				Cache::set( 'cwm:archives', $archives, HabariDateTime::HOUR * 12 );
+			}
+			
+			$block->archives = $archives;
+			
+		}
+		
+		public function action_block_content_cwm_flickr ( $block, $theme ) {
+			
+			if ( Cache::has( 'cwm:flickr_items' ) ) {
+				$items = Cache::get( 'cwm:flickr_items' );
+			}
+			else {
+				
+				$feed = file_get_contents( 'http://api.flickr.com/services/feeds/photos_public.gne?id=27041953@N00&lang=en-us&format=rss_200' );
+				
+				if ( !$feed ) {
+					return false;
+				}
+				
+				$xml = new SimpleXMLElement( $feed );
+				
+				if ( count( $xml->channel->item ) < 1 ) {
+					return false;
+				}
+				
+				$items = array();
+				
+				foreach ( $xml->channel->item as $item ) {
+					
+					if ( !preg_match('/<img src="([^"]+)/si', $item->description, $matches) ) {
+						continue;
+					}
+					
+					// for reference: large => _m, proportional => _t, square => _s
+					$suffix = '_s';
+					
+					$items[] = array(
+						'title' => (string)$item->title,
+						'link' => (string)$item->link,
+						'thumbnail' => str_replace( '_m.jpg', $suffix . '.jpg', $matches[1] ),
+						'image' => str_replace( '_m.jpg', '.jpg', $matches[1] ),
+						'description' => (string)$item->description,
+						'pubDate' => (string)$item->pubDate,
+						'guid' => (string)$item->guid,
+					);
+					
+				}
+				
+				// cache the output for 12 hours
+				Cache::set( 'cwm:flickr_items', $items, HabariDateTime::HOUR * 12 );
+				
+			}
+			
+			// shuffle the array
+			shuffle( $items );
+			
+			$return = array();
+			
+			// return 3 random objects
+			for ( $i = 0; $i < 3; $i++ ) {
+				// pop an item off the end of the array
+				$return[] = array_pop( $items );
+			}
+			
+			$block->items = $return;
 			
 		}
 		
