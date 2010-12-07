bb42792a43f5c27e8a076c7bf6d907b44a2746c3
Chris Meller <chris@doesnthaveone.com>
2010-12-07 15:24:53 -0500

    Add page selectors to the home template.

--- home.php
+++ home.php
@@ -7,6 +7,14 @@
 		include('entry.php');
 	}
 	
+	?>
+	
+		<div id="page-selector">
+			<?php echo $theme->prev_page_link(); ?> <?php echo $theme->page_selector( null, array( 'leftSide' => 2, 'rightSide' => 2 ) ); ?> <?php echo $theme->next_page_link(); ?>
+		</div>
+	
+	<?php
+	
 	$theme->display('footer');
 
 ?>
