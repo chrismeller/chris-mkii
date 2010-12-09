20bfeae0ba10642706ac66eb9e3d5eb08fa74264
Chris Meller <chris@doesnthaveone.com>
2010-12-09 14:02:25 -0500

    Add the page title generated in the theme and parse out the new archives array structure to generate nested ULs with years, months, days, and posts.

--- page.archives.php
+++ page.archives.php
@@ -2,12 +2,44 @@
 <?php 
 
 	$theme->display('header');
+	
+	?>
+	
+		<h1><?php echo $page_title; ?></h1>
+	
+	<?php
+	
+	echo '<ul id="archives" class="years">';
+	foreach ( $archives as $year => $months ) {
 		
-	foreach ( $posts as $post ) {
+		echo '<li class="year">' . $year;
 		
-		echo $post->title . '<br />';
+		echo '<ul class="months">';
+		foreach ( $months as $month => $days ) {
+			
+			echo '<li class="month">' . $month;
+			
+			echo '<ul class="days">';
+			foreach ( $days as $day => $posts ) {
+				
+				echo '<li class="day">' . $day;
+				
+				echo '<ul class="posts">';
+				foreach ( $posts as $post ) {
+					
+					echo '<li class="post"><a href="' . $post->permalink . '" title="' . _t( 'View post %s', array( $post->title ) ) . '">' . $post->title . '</a></li>';
+					
+				}
+				echo '</ul>';
+				
+			}
+			echo '</ul>';
+			
+		}
+		echo '</ul>';
 		
 	}
+	echo '</ul>';
 	
 	$theme->display( 'page_navigation' );
 	
