172ccf5684fdbdbfc3788e1b5887568f23152af4
Chris Meller <chris@doesnthaveone.com>
2010-12-13 21:15:39 -0500

    Add a template for the daily archives so that the "no posts for that x" message when you pick a bad day is correct and doesn't say month.

---
+++ year.month.day.php
@@ -0,0 +1,28 @@
+<?php if ( !defined( 'HABARI_PATH' ) ) { die('No direct access'); } ?>
+<?php 
+
+	$theme->display('header');
+	
+	?>
+	
+		<h1><?php echo $page_title; ?></h1>
+	
+	<?php
+	
+		if ( count( $posts ) == 0 ) {
+			?>
+			
+				<p>Sorry, there were no posts for that day.</p>
+			
+			<?php
+		}
+	
+		foreach ( $posts as $post ) {
+			include('entry.php');
+		}
+	
+	$theme->display( 'page_navigation' );
+	
+	$theme->display('footer');
+
+?>
