6fc1981140b33bf01353d7235d8b80d5f18eb23e
Chris Meller <chris@doesnthaveone.com>
2010-12-23 14:25:42 -0500

    Switch the licensing statement in the footer.

--- footer.php
+++ footer.php
@@ -12,7 +12,8 @@
 				<div id="colophon">
 				
 					<div id="copyright" class="span-22">
-						<p>Presented by Chris Meller. Licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/3.0/us/" title="Creative Commons Attribution 3.0 United States License">Creative Commons</a> license.</p>
+						<p>Presented by Chris Meller. Available under an <a rel="license" href="<?php echo URL::get( 'display_page', array( 'slug' => 'licensing' ) ); ?>">open source license</a>.</p>
+						<!--<p>Presented by Chris Meller. Licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/3.0/us/" title="Creative Commons Attribution 3.0 United States License">Creative Commons</a> license.</p>-->
 					</div>
 					
 					<div id="the-moose" class="span-2 last">
