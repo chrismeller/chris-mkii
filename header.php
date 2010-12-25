5894b401dc185de4d06163606123fb2a793a535e
Chris Meller <chris@doesnthaveone.com>
2010-12-25 01:12:15 -0500

    Switch to an HTML5 doctype and fix the meta content-type reference.

--- header.php
+++ header.php
@@ -1,8 +1,9 @@
 <?php if ( !defined( 'HABARI_PATH' ) ) { die('No direct access'); } ?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<!DOCTYPE html>
+<!--<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">-->
 <html>
 	<head>
-		<meta http-equiv="Content-Type" content="text/html">
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 		<title><?php echo $title; ?></title>
 		
 		<meta name="generator" content="Habari <?php echo Version::HABARI_VERSION; ?>">
