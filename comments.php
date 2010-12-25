4fc4a4479dc68fb8f82e3d36d46da28f158d1c8b
Chris Meller <chris@doesnthaveone.com>
2010-12-24 20:19:45 -0500

    Flesh out the comments display. It now displays a title, lists all current comments, and includes the comment form. With the exception of some styling, I think we're done.

--- comments.php
+++ comments.php
@@ -1,28 +1,99 @@
 <?php if ( !defined( 'HABARI_PATH' ) ) { die('No direct access'); } ?>
 
-<?php
-
-	if ( $post->info->comments_disabled != true ) {
-		
-		?>
-		
-			<div class="comments">
-				<h4 id="respond" class="reply"><?php echo _t( 'Leave a Reply', 'cwm' ); ?></h4>
-				
-				<?php
-				
-					if ( Session::has_messages() ) {
-						Session::messages_out();
-					}
+<div class="comments">
+	<h4 id="comments"><?php echo _t( 'Comments', 'cwm' ); ?></h4>
+	
+	<?php
+	
+		if ( $post->comments->moderated->count == 0 ) {
+			
+			?>
+			
+				<p class="no-comments"><?php echo _t( 'There are currently no comments.', 'cwm' ); ?></p>
+			
+			<?php
+			
+		}
+		else {
+			
+			?>
+			
+				<ol id="entry-comments">
+					<?php
 					
-					$post->comment_form()->out();
-				
-				?>
-				
-			</div>
+						foreach ( $post->comments->moderated as $comment ) {
+							
+							// figure out a bunch of values for the HTML
+							$id = 'comment_' . $comment->id;
+							
+							$class = array( 'comment' );
+							if ( $comment->status == Comment::status( 'unapproved' ) ) {
+								$class[] = 'unapproved';
+							}
+							$class = implode( ' ', $class );
+							
+							if ( $comment->url_out == '' ) {
+								$author = $comment->name_out;
+							}
+							else {
+								$author = '<a href="' . $comment->url_out . '" rel="external">' . $comment->name_out . '</a>';
+							}
+							
+							$meta = 'on <a href="' . $post->permalink . '#' . $id . '" title="' . _t( 'Comment Permalink', 'cwm' ) . '">' . $comment->date->format( 'F j, Y' ) . ' at ' . $comment->date->format( 'g:i a' ) . '</a>';
+							
+							?>
+							
+								<li id="<?php echo $id; ?>" class="<?php echo $class; ?>">
+									<span class="author"><?php echo $author; ?></span>
+									<span class="meta"><?php echo $meta; ?></span>
+									<div class="content">
+										<?php echo $comment->content_out; ?>
+									</div>
+								</li>
+							
+							<?php
+							
+						}
+						
+					?>
+				</ol>
+			
+			<?php
+			
+		}
 		
-		<?php
-		
-	}
-
-?>
+		// now for the comment form!
+		if ( $post->info->comments_disabled == true ) {
+			
+			?>
+			
+				<p><?php echo _t( 'Comments are disabled.', 'cwm' ); ?></p>
+			
+			<?php
+			
+		}
+		else {
+			
+			?>
+			
+				<div class="reply">
+					<h4 id="respond" class="reply"><?php echo _t( 'Leave a Reply', 'cwm' ); ?></h4>
+					
+					<?php
+					
+						if ( Session::has_messages() ) {
+							Session::messages_out();
+						}
+						
+						$post->comment_form()->out();
+					
+					?>
+					
+				</div>
+			
+			<?php
+			
+		}
+	
+	?>
+</div>
