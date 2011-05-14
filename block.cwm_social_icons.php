c08c4a2574e3f760b163e2463c098e6c3f69181d
Chris Meller <chris@doesnthaveone.com>
2011-05-13 20:06:21 -0400

    Fix the Habari Trac link so it doesn't include a hardcoded date.

--- block.cwm_social_icons.php
+++ block.cwm_social_icons.php
@@ -5,7 +5,7 @@
 	
 		<a href="https://github.com/chrismeller" id="github" class="tipsy" title="Github"><span>Github</span></a>
 		<a href="http://twitter.com/chrismeller" id="twitter" class="tipsy" title="Twitter"><span>Twitter</span></a>
-		<a href="http://trac.habariproject.org/habari?from=05%2F10%2F11&amp;daysback=90&amp;authors=chrismeller&amp;ticket=on&amp;ticket_details=on&amp;milestone=on&amp;changeset=on&amp;update=Update" id="habari" class="tipsy" title="Habari Project"><span>Habari Project</span></a>
+		<a href="http://trac.habariproject.org/habari?daysback=90&amp;authors=chrismeller&amp;ticket=on&amp;ticket_details=on&amp;milestone=on&amp;changeset=on&amp;update=Update" id="habari" class="tipsy" title="Habari Project"><span>Habari Project</span></a>
 		
 		<br class="clear" />
 	
