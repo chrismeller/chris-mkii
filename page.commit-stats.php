77e9fc06760a13980ae4463e362a3720d1030c0f
Chris Meller <chris@doesnthaveone.com>
2011-05-18 15:03:52 -0400

    Grab and graph the last 52 weeks of stats for the commit stats page.

--- page.commit-stats.php
+++ page.commit-stats.php
@@ -22,6 +22,24 @@
 	$last_update = HabariDateTime::date_create( $last_update );
 	$last_update = $last_update->format( 'F j, Y' ) . ' at ' . $last_update->format( 'g:i a' );
 	
+	// get our stats from the cache
+	$last_52 = Cache::get( 'cwm:commit_stats:stats_last_52' );
+	
+	
+	// we have some reformatting to do first
+	$last_52_commits = array();
+	$last_52_repos = array();
+	
+	foreach ( $last_52 as $week => $point ) {
+		
+		$last_52_commits[ $week ] = '[' . $week . ', ' . $point->commits . ']';
+		$last_52_repos[ $week ] = '[' . $week . ', ' . $point->repos . ']';
+		
+	}
+	
+	$last_52_commits = '[' . implode( ', ', $last_52_commits ) . ']';
+	$last_52_repos = '[' . implode( ', ', $last_52_repos ) . ']';
+	
 	?>
 		
 		<div id="post-<?php echo $post->id; ?>" class="<?php echo $theme->post_class( $post, $class ); ?>">
@@ -36,7 +54,19 @@
 			<div class="entry-content">
 				<?php echo $post->content_out; ?>
 			</div>
+			
+			<div id="placeholder-last-52" style="width: 680px; height: 200px;"></div>
+			<div id="placeholder-this-week" style="width: 680px; height: 200px;"></div>
+			
 		</div>
+		
+		<script type="text/javascript">
+			$( function() {
+				var last_52_commits = <?php echo $last_52_commits; ?>;
+				var last_52_repos = <?php echo $last_52_repos; ?>;
+				$.plot( $('#placeholder-last-52'), [ last_52_commits, last_52_repos ] );
+			} );
+		</script>
 		
 	<?php
 	
