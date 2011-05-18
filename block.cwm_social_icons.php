63f0ae9c413f8974d664e0e639b22bcd49368af1
Chris Meller <chris@doesnthaveone.com>
2011-05-18 13:04:46 -0400

    Use the new dotipsy class on the social icons instead of the old 'tipsy'.

--- block.cwm_social_icons.php
+++ block.cwm_social_icons.php
@@ -3,9 +3,9 @@
 	
 	<div class="wrap">
 	
-		<a href="https://github.com/chrismeller" id="github" class="tipsy" title="Github"><span>Github</span></a>
-		<a href="http://twitter.com/chrismeller" id="twitter" class="tipsy" title="Twitter"><span>Twitter</span></a>
-		<a href="http://trac.habariproject.org/habari?daysback=90&amp;authors=chrismeller&amp;ticket=on&amp;ticket_details=on&amp;milestone=on&amp;changeset=on&amp;update=Update" id="habari" class="tipsy" title="Habari Project"><span>Habari Project</span></a>
+		<a href="https://github.com/chrismeller" id="github" class="dotipsy" title="Github"><span>Github</span></a>
+		<a href="http://twitter.com/chrismeller" id="twitter" class="dotipsy" title="Twitter"><span>Twitter</span></a>
+		<a href="http://trac.habariproject.org/habari?daysback=90&amp;authors=chrismeller&amp;ticket=on&amp;ticket_details=on&amp;milestone=on&amp;changeset=on&amp;update=Update" id="habari" class="dotipsy" title="Habari Project"><span>Habari Project</span></a>
 		
 		<br class="clear" />
 	
