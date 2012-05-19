03be64f38e9f712e3cd778447b772dfe1ff39f42
Chris Meller <chris@doesnthaveone.com>
2012-05-19 12:48:01 -0400

    Add the BuzzData social icon, replacing the commit stats icon that hasn't worked forever anyway.

--- block.cwm_social_icons.php
+++ block.cwm_social_icons.php
@@ -1,16 +1,4 @@
 <?php if ( !defined( 'HABARI_PATH' ) ) { die('No direct access'); } ?>
-<?php 
-
-	if ( Cache::has( 'cwm:commit_stats:stats_total_this_week' ) ) {
-		$this_week = Cache::get( 'cwm:commit_stats:stats_total_this_week' );
-		
-		$this_week = '<a href="' . Site::get_url('habari') . '/commit-stats" id="commits-this-week" class="dotipsy" title="Commits this Week"><span>' . $this_week->commits . '</span></a>';
-	}
-	else {
-		$this_week = '';
-	}
-
-?>
 
 <div id="social-icons" class="">
 	
@@ -19,7 +7,7 @@
 		<a href="https://github.com/chrismeller" id="github" class="dotipsy" title="Github"><span>Github</span></a>
 		<a href="http://twitter.com/chrismeller" id="twitter" class="dotipsy" title="Twitter"><span>Twitter</span></a>
 		<a href="http://trac.habariproject.org/habari?daysback=90&amp;authors=chrismeller&amp;ticket=on&amp;ticket_details=on&amp;milestone=on&amp;changeset=on&amp;update=Update" id="habari" class="dotipsy" title="Habari Project"><span>Habari Project</span></a>
-		<?php echo $this_week; ?>
+		<a href="http://buzzdata.com/chrismeller" id="buzzdata" class="dotipsy" title="BuzzData"><span>BuzzData</span></a>
 		
 		<br class="clear" />
 	
